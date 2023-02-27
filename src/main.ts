e513cf1bb1cc6ba88ab3a5f95ae41e1c1cc917b4
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-02-27 15:17:34 +0700

    :sparkles: setup vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,27 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
-import './assets/main.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+const vuetify = createVuetify({
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+          mdi,
+        }
+      },
+  components,
+  directives,
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(router).use(vuetify)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,51 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+   
+<v-row justify="space-around">
+    <v-icon
+      size="large"
+      color="green-darken-2"
+    >
+      mdi-domain
+    </v-icon>
+
+    <v-icon
+      size="large"
+      color="blue-darken-2"
+    >
+      mdi-message-text
+    </v-icon>
+
+    <v-icon
+      size="large"
+      color="purple-darken-2"
+    >
+      mdi-dialpad
+    </v-icon>
+
+    <v-icon
+      size="large"
+      color="teal-darken-2"
+    >
+      mdi-email
+    </v-icon>
+
+    <v-icon
+      size="large"
+      color="blue-grey-darken-2"
+    >
+      mdi-call-split
+    </v-icon>
+
+    <v-icon
+      size="large"
+      color="orange-darken-2"
+    >
+      mdi-arrow-up-bold-box-outline
+    </v-icon>
+  </v-row>
   </main>
 </template>
