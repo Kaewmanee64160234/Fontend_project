05115e82aa9cd4e77f5fbc1fde9b628f9ebe95b3
Titapon Keawbua <118767390+64160047@users.noreply.github.com>
2023-03-01 02:42:25 +0700

    Add employee stores and type

---
+++ src/store/types/employee.type.ts
@@ -0,0 +1,17 @@
+export default interface  Employee{
+    id?:number;
+    name:string;
+    address:string;
+    tel:string;
+    email: string;
+    position: string;
+    hourly:number;
+    image?:string;
+    createdAt?:Date;
+    updateAt?:Date;
+    deleteAt?:Date;
+    
+ 
+ 
+ }
+ 
