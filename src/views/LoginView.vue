db421789de7ff035bc304b97f39160e7db104067
Pornchitar Sarsai <64160260@go.buu.ac.th>
2023-02-28 15:58:22 +0700

    rules in LoginForm

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+const loginName = ref("");
+const password = ref("");
 </script>
 
 <template>
   <v-app>
-    <v-main class="pa-5">
+    <v-main class="bg-blue-grey pa-5" >
       <v-card width="400px" class="mx-auto">
         <v-card-title
           primary-title
@@ -12,7 +15,30 @@
           Login
         </v-card-title>
         <v-card-text>
-          Form
+            <v-form >
+                <v-text-field
+                  v-model="loginName"
+                  label="Login Name"
+                  :rules="[
+                (v) => !!v || 'Email is required',
+                (v) =>
+                  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+                  'Email is not valid!!',
+              ]"
+              required
+                ></v-text-field>
+                <v-text-field
+                v-model="password"
+                  label="Password" type="password"
+                  :rules="[
+                (v) => !!v || 'Password is required',
+                (v) =>
+                  v.length >= 8 ||
+                  'Password must be more than or equal 8 characters',
+              ]"
+              required
+                ></v-text-field>
+              </v-form>
         </v-card-text>
         <v-card-actions class="justify-center">
           <v-btn color="success" >Login</v-btn>
