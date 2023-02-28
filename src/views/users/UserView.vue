ea166197ca2547ada8031e4e87f5b0853b95d645
Thanawuth Rodcharoen <64160049@go.buu.ac.th>
2023-02-28 22:19:30 +0700

    creat userview, user types

---
+++ src/views/users/UserView.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup> </script>
+<template>
+  <v-card>
+    <v-card-title>
+      Nutrition
+      <v-spacer></v-spacer>
+      <v-text-field
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+ 
+    ></v-data-table>
+  </v-card>
+</template>
