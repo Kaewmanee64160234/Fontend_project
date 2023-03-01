aa2fb944335c9fd41ff5112479086a568fc0afb6
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-03-01 16:03:33 +0700

    :safety_vest: edit mainmenu

--- src/components/menus/MainMenu.vue
+++ src/components/menus/MainMenu.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
+
+import { useAuthStore } from "@/stores/auth";
 import { ref } from "vue";
 const drawer = ref(true);
 const rail = ref(true);
+const authStore = useAuthStore();
 </script>
 
 <template>
-  
-    
+
     <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false"
       image="https://polycolors.ca/wp-content/uploads/2018/05/410604.jpg">
-      <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" title="John Leider" nav>
+      <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" title="hello" nav>
         <template v-slot:append>
           <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
         </template>
@@ -30,6 +32,8 @@ const rail = ref(true);
       to="/employee"></v-list-item>
       <v-list-item prepend-icon="mdi-basket-outline" title="Material" value="material"
       to="/material"></v-list-item>
+      <v-list-item prepend-icon="mdi-account-group-outline" title="Logout" value="material"
+      @click="authStore.logout"></v-list-item>
     </v-navigation-drawer>
 
 </template>
--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,27 +1,27 @@
-import { ref, computed } from "vue";
-import { defineStore } from "pinia";
-import auth from "@/services/auth";
-import router from "@/router";
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import auth from '@/services/auth'
+import router from '@/router'
 
-export const useAuthStore = defineStore("auth", () => {
-
-  const authName = ref("");
-
+export const useAuthStore = defineStore('auth', () => {
+  const authName = ref({})
   const login = async (email: string, password: string) => {
     try {
-      const res = await auth.login(email, password);
-      localStorage.setItem("token", res.data.access_token);
-      localStorage.setItem("user", JSON.stringify(res.data.user));
+      const res = await auth.login(email, password)
+      localStorage.setItem('token', res.data.access_token)
+      localStorage.setItem('user', JSON.stringify(res.data.user))
+      authName.value = JSON.parse(JSON.stringify(localStorage.getItem('user')))
     } catch (e) {
-        console.log(e);
+      console.log(e)
     }
-    router.push("/");
-  };
+    router.push('/')
+  }
   const logout = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-    router.replace("/login");
-  };
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+    authName.value = ''
+    router.replace('/login')
+  }
 
-  return { authName, login, logout };
-});
+  return { login, logout ,authName}
+})
