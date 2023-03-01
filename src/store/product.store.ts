945709185dd56107bea0943777b03a88e488eab9
Kittiyaporn Ngamlert <64160232@go.buu.ac.th>
2023-03-01 09:30:05 +0700

    :white_check_mark: can clear product form

--- src/store/product.store.ts
+++ src/store/product.store.ts
@@ -1,4 +1,4 @@
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { defineStore } from 'pinia'
 import type Product from './types/product.type';
 import axios from 'axios';
@@ -7,6 +7,14 @@ export const useProductStore = defineStore('Product', () => {
   const dialog = ref(false);
   const products = ref<Product[]>([]);
   const editedProduct = ref<Product>({ name: "", type: "-", size: "-", price: 0, image: 'no_image.jpg'});
+
+  watch(dialog, (newDialog, oldDialog) => {
+    console.log(newDialog);
+    if (!newDialog) {
+      editedProduct.value = { name: "", type: "-", size: "-", price: 0, image: 'no_image.jpg'};
+    }
+  });
+
   async function getProducts() {
     try {
       const res = await productService.getProducts();
@@ -25,5 +33,6 @@ export const useProductStore = defineStore('Product', () => {
       console.log(e);
     }
   }
-  return { products, getProducts, dialog, editedProduct, saveProduct }
+
+  return { products, getProducts, dialog, editedProduct, saveProduct}
 })
