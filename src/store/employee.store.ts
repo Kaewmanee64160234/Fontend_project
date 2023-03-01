0e7c21cf26c8ece9b3db4aae0c078fe5b6fc5194
Titapon Keawbua <118767390+64160047@users.noreply.github.com>
2023-03-01 23:41:01 +0700

    add message and loading

--- src/store/employee.store.ts
+++ src/store/employee.store.ts
@@ -2,7 +2,11 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import employeeService from '@/services/employee'
 import type Employee from './types/employee.type'
+import { useLoadingStore } from '@/store/loading'
+import { useMessageStore } from './message';
 export const useEmployeeStore = defineStore('employee', () => {
+  const messageStore = useMessageStore();
+  const loadingStore = useLoadingStore();
   const selected = ref<string[] | any[]>([])
   const dialog = ref(false)
   const allSelected = ref(false)
@@ -20,14 +24,18 @@ export const useEmployeeStore = defineStore('employee', () => {
   })
 
   const getEmployees = async () => {
+    loadingStore.isLoading = true;
     try {
       const res = await employeeService.getEmployees()
       employees.value = res.data
     } catch (err) {
       console.log(err)
+      messageStore.showError("ไม่สามารถดึงข้อมูล Employee ได้");
     }
+    loadingStore.isLoading = false;
   }
   const saveEmployee = async () => {
+    loadingStore.isLoading = true;
     try {
       if (!editEmployee.value.id) {
         await employeeService.createEmployee(editEmployee.value)
@@ -40,29 +48,57 @@ export const useEmployeeStore = defineStore('employee', () => {
       await getEmployees()
     } catch (err) {
       console.log(err)
+      messageStore.showError("ไม่สามารถบันทึกข้อมูล Employee ได้");
     }
+    loadingStore.isLoading = false;
   }
+  
   const editedEmployee = async (item: Employee) => {
+    loadingStore.isLoading = true
+
     editEmployee.value = JSON.parse(JSON.stringify(item))
     dialog.value = true
+    loadingStore.isLoading = false
   }
   const deleteEmployee = async (id: string) => {
-    await employeeService.deleteEmployee(id)
-    await getEmployees()
+    loadingStore.isLoading = true
+
+    try{
+
+      await employeeService.deleteEmployee(id)
+      await getEmployees()
+    }catch (err) {
+      messageStore.showError("ไม่สามารถdeleteข้อมูลพนักงานได้");
+    }
+    loadingStore.isLoading = false
+   
   }
   const selectEmployeeAll = async () => {
+    
+    loadingStore.isLoading = true
+
     if (!allSelected.value) {
       selected.value = employees.value.map((employee) => employee.id + '')
     }
+    loadingStore.isLoading = false
   }
   const selectEmployee = () => {
     allSelected.value = false
   }
   const deleteEmployees = async () => {
-    for (let i = 0; i < selected.value.length; i++) {
-      await employeeService.deleteEmployee(selected.value[i])
-      await getEmployees()
+    try{
+      loadingStore.isLoading = true
+
+      for (let i = 0; i < selected.value.length; i++) {
+        await employeeService.deleteEmployee(selected.value[i])
+        await getEmployees()
+      }
+      loadingStore.isLoading = false
+    }catch(err){
+      messageStore.showError("ไม่สามารถdeleteข้อมูลพนักงานได้");
+
     }
+  
   }
 
   return {
@@ -80,3 +116,4 @@ export const useEmployeeStore = defineStore('employee', () => {
     editedEmployee
   }
 })
+
