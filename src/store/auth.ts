fa6a02dbbaab30078f2b247ba1cee450e6c7c9c7
Pornchitar Sarsai <64160260@go.buu.ac.th>
2023-03-01 19:12:57 +0700

    message for show error when get wrong user&Password

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -3,19 +3,22 @@ import { defineStore } from 'pinia'
 import auth from '@/services/auth'
 import router from '@/router'
 import { useLoadingStore } from "./loading";
+import { useMessageStore } from "./message";
 
 export const useAuthStore = defineStore('auth', () => {
   const authName = ref({})
   const loadingStore = useLoadingStore();
+  const messageStore = useMessageStore();
   const login = async (email: string, password: string) => {
+    loadingStore.isLoading = true;
     try {
-      loadingStore.isLoading = true;
       const res = await auth.login(email, password)
       localStorage.setItem('token', res.data.access_token)
       localStorage.setItem('user', JSON.stringify(res.data.user))
       authName.value = JSON.parse(JSON.stringify(localStorage.getItem('user')))
     } catch (e) {
       console.log(e)
+      messageStore.showError("Username หรือ Password ไม่ถูกต้อง");
     }
     loadingStore.isLoading = false;
     router.push('/')
