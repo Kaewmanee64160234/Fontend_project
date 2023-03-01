1d62a16ded909f3a628e4ded8dcd0a63d9ce1d3a
Kittiyaporn Ngamlert <64160232@go.buu.ac.th>
2023-03-01 12:27:19 +0700

    :lipstick: edit size product image and can select product

--- src/views/products/ProductView.vue
+++ src/views/products/ProductView.vue
@@ -1,67 +1,79 @@
 <script lang="ts" setup>
 import { useProductStore } from '@/store/product.store';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import ProductDialog from '../../components/product/ProductDialog.vue';
+import ConfirmDialog from '@/components/ConfirmDialog.vue';
 
 const productStore = useProductStore();
+const confirmDlg = ref();
 const url = import.meta.env.VITE_URL_PORT
 onMounted(async () => {
-    await productStore.getProducts();
+
+  await productStore.getProducts();
 });
+const deleteProduct = async (id:number) => {
+  await confirmDlg.value.openDialog("Pleases Confirm", `Do you want delete this product`,'Accept','Cancel');
+    productStore.deleteProduct(id);
+};
 </script>
 <template>
-    <ProductDialog></ProductDialog>
-    <v-container>
-  <v-card>
-    <v-card-title>
-      Products
-      <v-btn
-        class="mdi mdi-plus"
-        style="float: right; background-color: #8ad879; color: white" @click="productStore.dialog = true"
-        >Add New Product</v-btn>
-      <v-spacer>
-      </v-spacer>
-      <v-text-field
-      class="mt-5"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
-    </v-card-title>
+  <ConfirmDialog ref="confirmDlg"></ConfirmDialog>
+  <ProductDialog></ProductDialog>
+  <v-container>
+    <v-card>
+      <v-card-title>
+        Products
+        <v-btn class="mdi mdi-plus" style="float: right; background-color: #8ad879; color: white"
+          @click="productStore.dialog = true">Add New Product</v-btn>
+        <v-spacer>
+        </v-spacer>
+        <v-text-field  
+          style="width: 20%"
+          variant="solo"
+          color="deep-purple-accent-4"
+          class="mt-7"
+          density="compact"
+          append-inner-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details></v-text-field>
+      </v-card-title>
 
-    <v-table>
+      <v-table >
         <thead>
-            <tr>
-                <th>Id</th>
-                <th>Image</th>
-                <th>Name</th>
-                <th>Type</th>
-                <th>Price</th>
-                <th>Size</th>
-                <th>Operations</th>
-                
-            </tr>
+          <tr>
+            <th></th>
+            <th>Id</th>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Price</th>
+            <th>Size</th>
+            <th>Operations</th>
+
+          </tr>
         </thead>
         <tbody>
-            <tr style="text-align:center" v-for="item of productStore.products" :key="item.id">
-                <td>{{ item.id }}</td>
-                <td><v-avatar><v-img :src="`${url}/products/image/${item.image}`"></v-img
-                ></v-avatar></td>
-                <td>{{ item.name }}</td>
-                <td>{{ item.type }}</td>
-                <td>{{ item.price }}</td>
-                <td>{{ item.size }}</td>
-                <td>
-          <v-btn class="mr-5" @click="productStore.editProduct(item)"
-            >Edit </v-btn
-          ><v-btn @click="productStore.deleteProduct(item.id!)">Delete</v-btn>
-        </td>
+          <tr style="text-align:center" v-for="item of productStore.products" :key="item.id">
+            <td><v-checkbox
+                  class="d-flex pa-4"
+                  color="indigo"
+                ></v-checkbox></td>
+            <td>{{ item.id }}</td>
+            <td><v-avatar size="80"><v-img :src="`${url}/products/image/${item.image}`"></v-img></v-avatar></td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.type }}</td>
+            <td>{{ item.price }}</td>
+            <td>{{ item.size }}</td>
+            <td>
+              <v-btn color="yellow" class="mr-5"  @click="productStore.editProduct(item)">Edit </v-btn>
+              <v-btn color="#F55050" @click="deleteProduct(item.id!)">Delete</v-btn>
+            </td>
 
-            </tr>
+          </tr>
         </tbody>
-    </v-table>
+      </v-table>
 
-  </v-card>
-</v-container>
+    </v-card>
+  </v-container>
 </template>
