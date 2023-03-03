59902da62117ae512f6f6f8d2b68e896369fc833
Thanawuth Rodcharoen <64160049@go.buu.ac.th>
2023-03-03 17:39:29 +0700

    fixed user dialog again

--- src/views/users/UserView.vue
+++ src/views/users/UserView.vue
@@ -2,12 +2,21 @@
 import ConfirmDialog from '@/components/ConfirmDialog.vue';
 import UserDialog from '@/components/user/UserDialog.vue';
 import { useUserStore } from '@/store/user.store';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 const confirmDlg = ref();
 const userStore = useUserStore();
-
+const users = computed(() => {
+  if (!userStore.search) {
+    return userStore.users;
+  } else {
+    return userStore.users.filter((user) => {
+      return user.username.toLocaleLowerCase().includes(userStore.search
+      )
+    });
+  }
+});
 onMounted(async () => {
-    await userStore.getUsers();
+  await userStore.getUsers();
 });
 const deleteUser = async (id: number) => {
   await confirmDlg.value.openDialog(
@@ -25,46 +34,28 @@ const deleteAllUsers = async () => {
     'Accept',
     'Cancel'
   )
-  await userStore.deleteAllUser()
+  userStore.deleteAllUser()
 }
 </script>
 <template>
   <ConfirmDialog ref="confirmDlg"></ConfirmDialog>
   <UserDialog></UserDialog>
-   <VContainer>
+  <VContainer>
     <VCard>
       <VCardTitle>
         User
-        <VBtn
-          class="mdi mdi-plus"
-          style="float: right; color: white"
-          color="#8ad879"
-          @click="userStore.dialog = !userStore.dialog"
-          >Add new user</VBtn
-        >
+        <VBtn class="mdi mdi-plus" style="float: right; color: white" color="#8ad879"
+          @click="userStore.dialog = true">Add new user</VBtn>
         <VBtn class="mdi mr-2" style="float: right; color: white" color="red" @click="deleteAllUsers">Delete All</VBtn>
         <VSpacer> </VSpacer>
-        <VTextField
-          style="width: 20%"
-          variant="solo"
-          color="deep-purple-accent-4"
-          class="mt-7"
-          density="compact"
-          append-inner-icon="mdi-magnify"
-          label="Search"
-          single-line
-          hide-details
-        ></VTextField>
+        <VTextField style="width: 20%" variant="solo" color="deep-purple-accent-4" class="mt-7" density="compact"
+          append-inner-icon="mdi-magnify" label="Search" single-line hide-details v-model = "userStore.search"></VTextField>
         <VTable class="text-center mt-5">
           <thead>
             <tr>
               <th>
-                <VCheckbox
-                  class="d-flex pa-4"
-                  color="indigo"
-                  v-model="userStore.allSelected"
-                  @click="userStore.selectUserAll"
-                ></VCheckbox>
+                <VCheckbox class="d-flex pa-4" color="indigo" v-model="userStore.allSelected"
+                  @click="userStore.selectUserAll"></VCheckbox>
               </th>
               <th>ID</th>
               <th>Username</th>
@@ -74,28 +65,26 @@ const deleteAllUsers = async () => {
             </tr>
           </thead>
           <tbody>
-            <tr v-for="item of userStore.users" :key="item.id" class="text-center">
+            <tr v-for="item of users" :key="item.id" class="text-center">
               <td>
-                <VCheckbox
-                  class="d-flex pa-4"
-                  color="indigo"
-                  v-model="userStore.selected"
-                  @click="userStore.selectUser()"
-                  :value="item.id+''"
-                ></VCheckbox>
+                <VCheckbox class="d-flex pa-4" color="indigo" v-model="userStore.selected" @click="userStore.selectUser()"
+                  :value="item.id + ''"></VCheckbox>
               </td>
               <td>{{ item.id }}</td>
               <td>{{ item.username }}</td>
               <td>{{ item.login }}</td>
-              <td>{{ item.password }}</td>
               <td>{{ item.role }}</td>
               <td>
-                <VBtn class="mr-5" color="yellow" @click="userStore.editUser(item)"
-                  >Edit</VBtn
-                ><VBtn color="red" @click="deleteUser(item.id!)">Delete</VBtn>
+                <VBtn class="mr-5" color="yellow" @click="userStore.editUser(item)">Edit</VBtn>
+                <VBtn color="red" @click="deleteUser(item.id!)">Delete</VBtn>
               </td>
             </tr>
           </tbody>
+          <tbody v-if="users.length == 0" >
+          <tr>
+            <td colspan="7" class="text-center">No data</td>
+          </tr>
+        </tbody>
         </VTable>
       </VCardTitle>
     </VCard>
