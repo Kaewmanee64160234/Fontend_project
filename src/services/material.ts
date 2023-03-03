380defea195d42f25da90117a3c063b50dfda8dc
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-03-03 10:38:39 +0700

    :white_check_mark: fix error create material

--- src/services/material.ts
+++ src/services/material.ts
@@ -6,17 +6,7 @@ function getMaterials() {
     return http.get("/materials");
   }
   function saveMaterial(material: Material) {
-    const formData = new FormData();
-    formData.append("name", material.name);
-    formData.append("minquantity", `${material.minquantity}`);
-    formData.append("quantity", `${material.quantity}`);
-    formData.append("unit", `${material.unit}`);
-    formData.append("price_per_unit", `${material.price_per_unit}`);
-    return http.post("/materials", formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    });
+    return http.post("/materials", material);
 }
   
 function updateMaterial(id: number, material:Material) {
