9efdd1094ef38f706d3504986c5aa163fd5a617d
Pornchitar Sarsai <64160260@go.buu.ac.th>
2023-03-10 14:06:30 +0700

    menuProduct

--- src/components/MenuCard.vue
+++ src/components/MenuCard.vue
@@ -1,15 +1,27 @@
 <script lang="ts" setup>
+import { useProductStore } from '@/store/product.store';
+import { onMounted,ref } from 'vue';
+import type Product from '@/store/types/product.type';
 
-
+const orderList = ref<{product: Product; count: number; sum: number}[]>([]);
+const productStore = useProductStore();
+const backendURL = import.meta.env.VITE_BACKEND_URL;
+function addProduct(item:Product){
+  console.log(orderList.value);
+  orderList.value.push({product: item, count: 1, sum: 1* item.price});
+}
+onMounted(async () => {
+  await productStore.getProducts();
+})
 const props = defineProps({
   name: {
     type: String,
     required: true,
   },
-  cost: {
-    type: String,
-    required: true,
-  },
+  // cost: {
+  //   type: String,
+  //   required: true,
+  // },
   img: {
     type: String,
     required: true,
@@ -27,28 +39,41 @@ const props = defineProps({
 </script>
 
 <template >
-  <v-container>
-    <v-row>
-      <v-col>
-        <v-container>
-          <v-row>
-            <v-col cols="4">
-              <v-card width="400">
-                <v-card-item class="text-center">
-                  <v-card-title>{{ props.name }}</v-card-title>
-          
-                  <v-card-subtitle>{{ props.cost }}</v-card-subtitle>
-                </v-card-item>
-          
-                <v-card-text>
-                  <v-img :src="props.img" width="120px"></v-img>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-col>
-    </v-row>
-  </v-container>
-  
+  <a href="#" v-for="item of productStore.products" :key="item.id">
+  <div class="card item-card" >
+  <img 
+  class="card-img-top item-img"
+        :src="`${backendURL}/products/image/${props.img}`"
+        
+        width="120px"
+      />
+
+      <div class="card-body text-center">
+        <h5 class="card-title" >{{ item.name }}</h5>
+        <p class="card-text fontsm">{{ item.price }}</p>
+      </div>
+    </div>
+</a>
 </template>
+
+<style>
+.card-title{
+  font-size: 16px !important;
+  margin-bottom: 0;
+}
+.item-card .item-img{
+  height: 115px !important;
+  object-fit: cover;
+}
+a .item-card{
+  /* width: 120px !important; */
+  cursor:pointer;
+  color: black !important;
+}
+a:hover .item-card{
+  color: #1F3611  !important;;
+}
+a:link { text-decoration: none; }
+
+.fontsm { font-size:small}
+</style>
