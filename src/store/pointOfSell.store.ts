54f96ca94d35912fd4eb9534a14741cee4f0fde3
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-03-10 22:35:31 +0700

    create type OrderItem

--- src/store/pointOfSell.store.ts
+++ src/store/pointOfSell.store.ts
@@ -4,9 +4,10 @@ import { ref } from "vue";
 export const usePointOfSale = defineStore('point of sale', () => {
     const dialogPayment = ref(false);
     const dialogPrompypay = ref(false);
-
-
     const dialogPromotion = ref(false);
+    const order = ref([])
+
+
 
     return {dialogPayment, dialogPromotion,dialogPrompypay}
 
