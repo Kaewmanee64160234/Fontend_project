7c2e2640b2bcdb00eea911d2f64589c2b2bbc6f0
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-03-11 22:23:28 +0700

    fix template point of sale

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,7 +14,6 @@ const customerStore = useCustomerStore()
 const productStore = useProductStore()
 const pointOfSaleStore = usePointOfSale()
 
-
 const addToCart = (item: Product) => {
   pointOfSaleStore.updatetmpProduct(item)
   pointOfSaleStore.dialogTopping = true
@@ -38,8 +37,8 @@ const reduceAmoutProduct = (index: number) => {
       pointOfSaleStore.orderItemList[index].amount * pointOfSaleStore.orderItemList[index].price
   }
 }
-onMounted(()=>{
-  productStore.getProductByCatagory('2');
+onMounted(() => {
+  productStore.getProductByCatagory('2')
 })
 </script>
 
@@ -85,16 +84,19 @@ onMounted(()=>{
                   <th scope="col" class="text-center">จำนวน</th>
                   <th scope="col" class="text-center">ราคา</th>
                   <th scope="col" class="text-center">ราคารวม</th>
+                  <th scope="col" class="text-center">เพิ่มเติม</th>
                   <th></th>
                 </tr>
               </thead>
               <tbody>
                 <tr v-if="pointOfSaleStore.orderItemList.length === 0">
-                  <td style="text-align: center" colspan="5">No data</td>
+                  <td style="text-align: center" colspan="6">No data</td>
                 </tr>
                 <tr v-else v-for="(item, index) of pointOfSaleStore.orderItemList" :key="index">
                   <td style="text-align: center">{{ index + 1 }}</td>
                   <td scope="col" class="text-center">{{ item.name }}</td>
+                  <td class="text-center">{{ item.amount }}</td>
+                  <td class="text-center">{{ item.price }}</td>
                   <td class="text-center">
                     <v-btn
                       color="secondary"
@@ -112,18 +114,8 @@ onMounted(()=>{
                       @click="reduceAmoutProduct(index)"
                     ></v-btn>
                   </td>
-                  <td class="text-center">
-                    {{ item.price }}
-                  </td>
-                  <td class="text-center">{{ item.total }}</td>
-                  <td>
-                    <v-btn
-                      color="red"
-                      icon="mdi-delete"
-                      size="x-small"
-                      @click="deleteOrderItem(index)"
-                    ></v-btn>
-                  </td>
+                  <td class="text-center">addOn</td>
+                  <td><v-btn color="red" icon="mdi-delete" size="x-small"></v-btn></td>
                 </tr>
               </tbody>
             </table>
