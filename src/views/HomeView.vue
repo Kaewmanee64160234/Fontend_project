3ac614973015db3ef9bea9e8314430affca5c2e1
Thanawuth Rodcharoen <64160049@go.buu.ac.th>
2023-03-11 21:01:01 +0700

    delete calculate store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 <script setup lang="ts">
 import { useMenuStore } from '@/store/menu'
 import { onMounted, ref } from 'vue'
@@ -13,22 +11,11 @@ import { useCustomerStore } from '@/store/customer.store'
 import { useProductStore } from '@/store/product.store'
 import type Product from '@/store/types/product.type'
 import type { OrderItem } from '@/store/types/orderItem.type'
-import { useCalculateStore } from '@/store/calculate.store'
-
-const calculateStore = useCalculateStore()
+import { computed } from '@vue/reactivity'
 const customerStore = useCustomerStore()
 const productStore = useProductStore()
 const menuStore = useMenuStore()
 const pointOfSaleStore = usePointOfSale()
-=======
-<script setup lang="ts">import { useMenuStore } from '@/store/menu';
-import { onMounted } from 'vue';
-import MenuCard from "@/components/MenuCard.vue";
-import { useProductStore } from '@/store/product.store';
-const productStore = useProductStore();
-const menuStore =  useMenuStore();
->>>>>>> parent of 8277332 (add calculate)
-
 onMounted(async () => {
   await productStore.getProducts()
   menuStore.menuFilter('Drinks')
@@ -38,94 +25,15 @@ const addToCart = (item: Product) => {
   pointOfSaleStore.updatetmpProduct(item)
   pointOfSaleStore.dialogTopping = true;
 }
+const total = computed(() => {
+  return pointOfSaleStore.CaltotalPrice();
+})
 </script>
 
 <template>
   <div class="content-area">
     <DialogPayment></DialogPayment>
     <div class="content">
-
-        <div class="row">
-          <div class="col-md-6 item-side">
-            <div class="row-md-6">
-              <v-tabs fixed-tabs color="#9F8772" dark >
-                <v-tab @click="menuStore.menuFilter('Drinks')">
-                  เครื่องดื่ม
-                </v-tab>
-                <v-tab @click="menuStore.menuFilter('Foods')">
-                  อาหาร
-                </v-tab>
-                <v-tab @click="menuStore.menuFilter('Desserts')">
-                  ของหวาน
-                </v-tab>
-              </v-tabs>
-            </div>
-            <div class="row">
-              <div class="col-md-3 mb-2 mt-4" v-for="item in productStore.products" :key="item.id">
-                <MenuCard :name="item.name" :cost="item.price" :type="'Hello'" :img="item.image!" :price="item.price"></MenuCard>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-6 mt-2">
-            <div class="table-responsive cart-table">
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col" class="text-center">รายการ</th>
-                    <th scope="col" class="text-center">ราคา</th>
-                    <th scope="col" class="text-center">จำนวน</th>
-                    <th scope="col" class="text-center">รวม</th>
-                    <th scope="col" class="text-center">เพิ่มเติม</th>
-                    <th></th>
-                    <th></th>
-                    
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td style="text-align: center;"></td>
-                    <td scope="col" class="text-center"></td>
-                    <td class="text-center"></td>
-                    <td class="text-center"></td>
-                  </tr>
-                </tbody>
-              </table>
-              </div>
-            
-
-            <div class="summary mt-2">
-              <div class="row">
-                <div class="col-md-7">
-                  <div class="d-flex justify-content-between">
-                    <p class="fw-bold mb-0">ราคารวม :</p>
-                    <p class="fw-bold mb-0">บาท</p>
-                  </div>
-                  <div class="d-flex justify-content-between">
-                    <p class="fw-bold mb-0">ส่วนลด :</p>
-                    <p class="fw-bold mb-0">บาท</p>
-                  </div>
-                  <div class="d-flex justify-content-between">
-                    <p class="fw-bold mb-0">ยอดที่ต้องชำระ :</p>
-                    <p class="fw-bold mb-0">บาท</p>
-                  </div>
-                  <div class="d-flex justify-content-between">
-                    <p class="fw-bold mb-0">ยอดรับชำระ :</p>
-                    <p class="fw-bold mb-0">บาท</p>
-                  </div>
-                  <div class="d-flex justify-content-between">
-                    <p class="fw-bold mb-0">จำนวนเงินที่ทอน :</p>
-                    <p class="fw-bold mb-0">บาท</p>
-                  </div>
-                </div>
-
-                <div class="col-md-5">
-                  <span class="fw-bold mt-2">ระบุจำนวนเงินที่ได้รับ</span>
-                  <input class="form-control" id="amount" type="text" placeholder="Amount"/>
-                  <v-btn color="#E9A178" width="inherit" class="mt-5">Calculator</v-btn>
-                </div>
-              </div>
-
       <FindMemberDialog></FindMemberDialog>
       <PromotionDialog></PromotionDialog>
       <TestDialog></TestDialog>
@@ -141,17 +49,9 @@ const addToCart = (item: Product) => {
           </div>
           <div class="row">
             <div class="col-md-3 mb-2 mt-4" v-for="item in productStore.products" :key="item.id">
-             
-              <MenuCard
-                :name="item.name"
-                :cost="item.price"
-                :type="item.type + ''"
-                :img="item.image!"
-                :price="item.price"
-                :catagory-id="item.catagoryId + ''"
-                @click="addToCart(item)"
-              ></MenuCard>
-              
+
+              <MenuCard :name="item.name" :cost="item.price" :type="item.type + ''" :img="item.image!" :price="item.price"
+                :catagory-id="item.catagoryId + ''" @click="addToCart(item)"></MenuCard>
 
             </div>
           </div>
@@ -189,36 +89,32 @@ const addToCart = (item: Product) => {
               <div class="col-md-7">
                 <div class="d-flex justify-content-between">
                   <p class="fw-bold mb-0">ราคารวม :</p>
-                  <p class="fw-bold mb-0">บาท</p>
+                  <p class="fw-bold mb-0">{{ total
+                  }} บาท</p>
                 </div>
                 <div class="d-flex justify-content-between">
                   <p class="fw-bold mb-0">ส่วนลด :</p>
-                  <p class="fw-bold mb-0">บาท</p>
+                  <p class="fw-bold mb-0">{{ pointOfSaleStore.total_dicount }} บาท</p>
                 </div>
                 <div class="d-flex justify-content-between">
                   <p class="fw-bold mb-0">ยอดที่ต้องชำระ :</p>
-                  <p class="fw-bold mb-0">บาท</p>
+                  <p class="fw-bold mb-0">{{ pointOfSaleStore.totalAndDicount }} บาท</p>
                 </div>
                 <div class="d-flex justify-content-between">
                   <p class="fw-bold mb-0">ยอดรับชำระ :</p>
-                  <p class="fw-bold mb-0">บาท</p>
+                  <p class="fw-bold mb-0">{{ pointOfSaleStore.totalAndDicount }} บาท</p>
                 </div>
                 <div class="d-flex justify-content-between">
                   <p class="fw-bold mb-0">จำนวนเงินที่ทอน :</p>
-                  <p class="fw-bold mb-0">บาท</p>
+                  <p class="fw-bold mb-0">{{ pointOfSaleStore.change_money }} บาท</p>
                 </div>
               </div>
 
               <div class="col-md-5">
                 <span class="fw-bold mt-2">ระบุจำนวนเงินที่ได้รับ</span>
                 <input class="form-control" id="amount" type="text" placeholder="Amount" />
-                <v-btn
-                  color="#E9A178"
-                  width="inherit"
-                  class="mt-5"
-                  @click="pointOfSaleStore.dialogPayment = true"
-                  >Payment</v-btn
-                >
+                <v-btn color="#E9A178" width="inherit" class="mt-5"
+                  @click="pointOfSaleStore.dialogPayment = true">Payment</v-btn>
               </div>
             </div>
           </div>
@@ -226,25 +122,15 @@ const addToCart = (item: Product) => {
             <div class="row">
               <div class="col-md-6">
                 <div class="d-flex justify-content-between">
-                  <v-btn
-                    color="#E9A178"
-                    class="mt-5"
-                    @click="pointOfSaleStore.dialogPromotion = true"
-                    >Promotion</v-btn
-                  >
+                  <v-btn color="#E9A178" class="mt-5" @click="pointOfSaleStore.dialogPromotion = true">Promotion</v-btn>
                   <v-btn color="#E9A178" class="mt-5">Clear All</v-btn>
                 </div>
                 <br />
                 <div class="d-flex justify-content-between">
-                  <v-btn color="#E9A178" class="mt-5" @click="customerStore.dialog = true"
-                    >Find Member</v-btn
-                  >
+                  <v-btn color="#E9A178" class="mt-5" @click="customerStore.dialog = true">Find Member</v-btn>
                   <v-btn color="#E9A178" class="mt-5">Save</v-btn>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
             </div>
           </div>
         </div>
