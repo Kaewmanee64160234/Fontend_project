8cb55af53587921ac241f9ce6ea84787909898a1
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-03-11 17:34:39 +0700

    add loadingStore in order store

--- src/store/order.store.ts
+++ src/store/order.store.ts
@@ -2,9 +2,11 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import type { Order } from '@/store/types/Order.type'
 import orderService from '@/services/order'
+import { useLoadingStore } from './loading'
 
 export const useOrderStore = defineStore('order', () => {
   const orders = ref<Order[]>([])
+  const loadingStore = useLoadingStore();
   const tempOrder = ref<Order>({
     customerId: 1,
     discount: 0,
@@ -14,13 +16,21 @@ export const useOrderStore = defineStore('order', () => {
     payment: 'promptpay'
   })
   const getOrders = async () => {
+    loadingStore.isLoading = true
+
     const response = await orderService.getOrders()
     orders.value = response.data
+    loadingStore.isLoading = false
+
   }
 
   const getOneOrder = async (id: string) => {
+    loadingStore.isLoading = true
+
     const response = await orderService.getOneOrder(id)
     tempOrder.value = response.data;
+    loadingStore.isLoading = false
+
   }
   return { getOrders, orders, getOneOrder,tempOrder }
 })
