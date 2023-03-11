e8d4e02ef9c9a4fb5a91eaa8f35150c5e6d36075
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-03-11 17:37:12 +0700

    add messageStore in order store

--- src/store/order.store.ts
+++ src/store/order.store.ts
@@ -3,10 +3,12 @@ import { ref } from 'vue'
 import type { Order } from '@/store/types/Order.type'
 import orderService from '@/services/order'
 import { useLoadingStore } from './loading'
+import { useMessageStore } from './message'
 
 export const useOrderStore = defineStore('order', () => {
   const orders = ref<Order[]>([])
-  const loadingStore = useLoadingStore();
+  const loadingStore = useLoadingStore()
+  const messageStore = useMessageStore();
   const tempOrder = ref<Order>({
     customerId: 1,
     discount: 0,
@@ -17,20 +19,30 @@ export const useOrderStore = defineStore('order', () => {
   })
   const getOrders = async () => {
     loadingStore.isLoading = true
+    try {
+      const response = await orderService.getOrders()
+      orders.value = response.data
+    } catch (err) {
+      console.log(err)
+      messageStore.showError("ไม่สามารถดึงข้อมูลได้");
 
-    const response = await orderService.getOrders()
-    orders.value = response.data
+    }
+
     loadingStore.isLoading = false
-
   }
 
   const getOneOrder = async (id: string) => {
     loadingStore.isLoading = true
+    try {
+      const response = await orderService.getOneOrder(id)
+      tempOrder.value = response.data
+    } catch (err) {
+      console.log(err)
+      messageStore.showError("ไม่สามารถดึงข้อมูลได้");
 
-    const response = await orderService.getOneOrder(id)
-    tempOrder.value = response.data;
+    }
+
     loadingStore.isLoading = false
-
   }
-  return { getOrders, orders, getOneOrder,tempOrder }
+  return { getOrders, orders, getOneOrder, tempOrder }
 })
