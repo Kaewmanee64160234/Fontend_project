0147f0f928b64d9dbd91f2da7e3e26b6b577228c
Titapon Keawbua <118767390+64160047@users.noreply.github.com>
2023-03-14 21:05:07 +0700

    new employee login

--- src/views/EmployeeLogin.vue
+++ src/views/EmployeeLogin.vue
@@ -8,48 +8,63 @@ const form = ref<InstanceType<typeof VForm> | null>(null)
 const email = ref('')
 const name = ref('')
 const login = async () => {
-  const { valid } = await form.value!.validate()
-  if (valid) {
-    employeeStore.loginEmployee(email.value, name.value)
-  }
+    const { valid } = await form.value!.validate()
+    if (valid) {
+        employeeStore.loginEmployee(email.value, name.value)
+    }
 }
 const clear = () => {
-  form.value?.reset();
+    form.value?.reset();
 }
 </script>
 
 <template>
-  <v-app>
-    <v-main class="bg-blue-grey pa-5">
-      <v-card width="400px" class="mx-auto">
-        <v-img
-          src="https://i.pinimg.com/564x/e2/90/49/e290496e2521d29d3ac34af6e5d18c42.jpg"
-          height="200px"
-          cover
-        ></v-img>
-        <v-card-title primary-title class="text-center"> Login Employee</v-card-title>
-        <v-card-text>
-          <v-form ref="form" v-model="valid">
-            <v-text-field
-              v-model="name"
-              label="Login Name"
-              :rules="[(v) => !!v || 'Email is required']"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="email"
-              label="email"
-              type="text"
-              :rules="[(v) => !!v || 'email is required']"
-              required
-            ></v-text-field>
-          </v-form>
-        </v-card-text>
-        <v-card-actions class="justify-center">
-          <v-btn color="success" @click="login">Login</v-btn>
-          <v-btn color="error" @click="clear">Clear</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-main>
-  </v-app>
+    <v-container>
+        <v-card>
+            <v-card-title>
+                Employee
+                <v-spacer>
+                    <v-table class="text-center mt-5">
+                        <v-spacer>
+                            <td>
+                                <v-avatar color="info">
+                                    <v-icon icon="mdi-account-circle"></v-icon>
+                                </v-avatar>
+                            </td>
+
+                        </v-spacer>
+                    </v-table>
+                </v-spacer>
+
+                <v-spacer> </v-spacer>
+                <v-table class="text-center mt-5">
+                    <tbody>
+                        <tr class="text-center">
+                            <v-text-field style=""></v-text-field>
+
+                        </tr>
+                        <tr class="text-center">
+                            <v-text-field style=""></v-text-field>
+
+                        </tr>
+                        <tr class="text-center">
+                            <v-text-field style=""></v-text-field>
+
+                        </tr>
+
+
+                        <td>
+                            <v-btn color="green" style="float: right; ">Login</v-btn>
+                        </td>
+                    </tbody>
+
+
+
+                </v-table>
+            </v-card-title>
+        </v-card>
+    </v-container>
 </template>
+
+
+
