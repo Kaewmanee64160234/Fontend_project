fd998486ada6a45c93b1c938e0b85a6ccd110342
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-03-16 18:37:29 +0700

    can get data employee from local stprage

--- src/store/types/user.type.ts
+++ src/store/types/user.type.ts
@@ -1,9 +1,12 @@
+import type Employee from "./employee.type";
+
 export default interface User {
     id?: number;
     username: string;
     login: string;
     password: string;
     role: string;
+    employee?: Employee;
     createdAt?: Date;
     updatedAt?: Date;
     deletedAt?: Date;
