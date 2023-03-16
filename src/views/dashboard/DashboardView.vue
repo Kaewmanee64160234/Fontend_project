043b78c3e5425ed3f249a855270802b501de56c2
Kittiyaporn Ngamlert <64160232@go.buu.ac.th>
2023-03-16 14:02:51 +0700

    :memo: edit layout dashboard

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -9,7 +9,42 @@ import ChartComponentDialog from "../../components/dashboards/ChartComponentDial
       <v-card-title>
         Dashbords
         </v-card-title>
-        <ChartComponentDialog></ChartComponentDialog>
+        <v-row>
+          <v-col cols="12" sm="4" md="5">
+            <ChartComponentDialog></ChartComponentDialog>
+          </v-col>
+          <v-col cols="12" sm="4" md="5">
+            <ChartComponentDialog></ChartComponentDialog>
+            </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" sm="4" md="5">
+            สินค้าขายดี
+          </v-col>
+          <v-col cols="12" sm="4" md="5">
+            สินค้ายอดขายน้อย
+          </v-col>
+          <v-col cols="12" sm="4" md="5">
+            <v-table>
+              <thead style="text-align: center">สินค้าใกล้หมดสต็อก
+                <tr>
+                  <th scope="col" class="text-center">No.</th>
+                  <th scope="col" class="text-center">ชื่อ</th>
+                  <th scope="col" class="text-center">จำนวน</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td style="text-align: center">1</td>
+                  <td style="text-align: center">คุกกี้ </td>
+                  <td style="text-align: center">4 </td>
+                </tr>
+              </tbody>
+            </v-table>
+          </v-col>
+        </v-row>
+       
+        
         </v-card>
         </v-container>
 </template>
