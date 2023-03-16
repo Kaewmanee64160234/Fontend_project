aa107d4601fe9e12981bf198e06757ad64d79103
Titapon Keawbua <118767390+64160047@users.noreply.github.com>
2023-03-16 20:47:33 +0700

    fix Enployeelogin btn and table

--- src/views/EmployeeLogin.vue
+++ src/views/EmployeeLogin.vue
@@ -11,76 +11,72 @@ const form = ref<InstanceType<typeof VForm> | null>(null)
 const email = ref('')
 const name = ref('')
 const data = ref(JSON.parse(JSON.stringify(localStorage.getItem('employee'))))
-const employee = ref<Employee >(JSON.parse(data.value))
+const employee = ref<Employee>(JSON.parse(data.value))
 
 onMounted(async () => {
-   await employeeStore.getOneEmployee(employee.value.id+'')
-   console.log(employeeStore.editEmployee)
+    await employeeStore.getOneEmployee(employee.value.id + '')
+    console.log(employeeStore.editEmployee)
 });
 </script>
 
 <template>
-  <v-container>
-    <v-card style="background-color: #f1dec9">
-      <v-card-title>
-        Employee
-        <v-spacer> </v-spacer>
+    <v-container>
+        <v-card style="background-color: #f1dec9">
+            <v-card-title>
+                Employee
+                <v-spacer> </v-spacer>
 
-        <v-table class="text-center mt-5" style="background-color: #f1dec9">
-          <v-row>
-            <v-col colspan="2">
-              <v-avatar size="80"
-                ><v-img :src="`${url}/employees/image/${employee.image}`"></v-img
-              ></v-avatar>
-              <div class="d-flex align-center flex-column">
-                <div class="text-subtitle-2 mt-3">{{ employee.name }}</div>
-                <v-card class="mt-5" width="350" text="This is content">
-                  <v-card-title>
-                    <div class="text-left">
-                      <p>ชื่อ : {{ employee.name }}</p>
-                      <p>email : {{ employee.email }}</p>
-                      <p>ตำแหน่ง : {{ employee.position }}</p>
-                    </div>
-                  </v-card-title>
-                </v-card>
-              </div>
-            </v-col>
-          </v-row>
-        </v-table>
+                <v-table class="text-center mt-5" style="background-color: #f1dec9">
+                    <v-row>
+                        <v-col colspan="2">
+                            <v-avatar size="80"><v-img :src="`${url}/employees/image/${employee.image}`"></v-img></v-avatar>
+                            <div class="d-flex align-center flex-column">
+                                <div class="text-subtitle-2 mt-3">{{ employee.name }}</div>
+                                <v-card class="mt-5" width="350" text="Pofile">
+                                    <v-card-title>
+                                        <div class="text-left">
+                                            <p>ชื่อ : {{ employee.name }}</p>
+                                            <p>email : {{ employee.email }}</p>
+                                            <p>ตำแหน่ง : {{ employee.position }}</p>
+                                        </div>
+                                    </v-card-title>
+                                </v-card>
+                            </div>
+                        </v-col>
+                    </v-row>
+                </v-table>
 
-        <v-table height="400px">
-          <thead style="background-color: #f1dec9">
-            <tr>
-              <td></td>
+                <v-table height="400px">
+                    <thead style="background-color: #f1dec9">
+                        <tr>
+                            <td></td>
+                            <td></td>
 
-              <td >
-                <v-btn class="mdi mr-2 mdi-delete" style="float: right; color: white" color="red"
-                  >Checkout</v-btn
-                >
-                <v-btn class="mdi mr-2 mdi-delete" style="float: right; color: white" color="blue"
-                  >Checkin</v-btn
-                >
-              </td>
-            </tr>
-          </thead>
-          <thead>
-            <tr 
-            >
-            <td class="text-center mr-5">Time in</td>
-              <td class="text-center mr-5">Time out</td>
-              <td class="text-center mr-5">Total hour</td>
-              
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(item,index) in employeeStore.editEmployee.check_in_outs" :key="index">
-              <td>{{ item.time_in}}</td>
-              <td>{{ item.time_out}}</td>
-              <td>{{ item.total_hour}}</td>
-            </tr>
-          </tbody>
-        </v-table>
-      </v-card-title>
-    </v-card>
-  </v-container>
+                            <td>
+                                <v-btn class="mdi mr-2 mdi-delete" style="float: right; color: white"
+                                    color="red">Checkout</v-btn>
+                                <v-btn class="mdi mr-2 mdi-delete" style="float: right; color: white"
+                                    color="blue">Checkin</v-btn>
+                            </td>
+                        </tr>
+                    </thead>
+                    <thead>
+                        <tr>
+                            <td class="text-center mr-5">Time in</td>
+                            <td class="text-center mr-5">Time out</td>
+                            <td class="text-center mr-5">Total hour</td>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="text-center mr-5" v-for="(item, index) in employeeStore.editEmployee.check_in_outs" :key="index">
+                            <td>{{ item.time_in }}</td>
+                            <td>{{ item.time_out }}</td>
+                            <td>{{ item.total_hour }}</td>
+                        </tr>
+                    </tbody>
+                </v-table>
+            </v-card-title>
+        </v-card>
+    </v-container>
 </template>
