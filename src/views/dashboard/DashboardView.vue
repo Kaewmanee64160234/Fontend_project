f28e8a72e61873d90bec5f88ee097c4e54b4ce35
Kittiyaporn Ngamlert <64160232@go.buu.ac.th>
2023-03-16 13:43:36 +0700

    :wastebasket: delete BoarddayDialog

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -1,7 +1,6 @@
 
 <script lang="ts" setup>
 import BoarddayDialog from "../../components/dashboards/BoardmonthDialog.vue"
-import BoardmonthDialog from "../../components/dashboards/BoardmonthDialog.vue"
 </script>
 
 <template>
@@ -10,8 +9,42 @@ import BoardmonthDialog from "../../components/dashboards/BoardmonthDialog.vue"
       <v-card-title>
         Dashbords
         </v-card-title>
-        <BoarddayDialog></BoarddayDialog>
-        <BoardmonthDialog></BoardmonthDialog>
+        <v-row>
+          <v-col cols="12" sm="4" md="5">
+            <BoarddayDialog></BoarddayDialog>
+          </v-col>
+          <v-col cols="12" sm="4" md="5">
+            <BoarddayDialog></BoarddayDialog>
+            </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" sm="4" md="5">
+            สินค้าขายดี
+          </v-col>
+          <v-col cols="12" sm="4" md="5">
+            สินค้ายอดขายน้อย
+          </v-col>
+          <v-col cols="12" sm="4" md="5">
+            <v-table>
+              <thead style="text-align: center">สินค้าใกล้หมดสต็อก
+                <tr>
+                  <th scope="col" class="text-center">No.</th>
+                  <th scope="col" class="text-center">ชื่อ</th>
+                  <th scope="col" class="text-center">จำนวน</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td style="text-align: center">1</td>
+                  <td style="text-align: center">คุกกี้ </td>
+                  <td style="text-align: center">4 </td>
+                </tr>
+              </tbody>
+            </v-table>
+          </v-col>
+        </v-row>
+       
+        
         </v-card>
         </v-container>
 </template>
