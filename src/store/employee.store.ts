2926a0a76b20831f051f8d25ceabc9e5d408089d
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-03-19 17:23:12 +0700

    new page for check in out emp

--- src/store/employee.store.ts
+++ src/store/employee.store.ts
@@ -135,8 +135,9 @@ export const useEmployeeStore = defineStore('employee', () => {
   }
   const getOneSummarySalaryEmp = async (id: string) => {
     const res = await employeeService.getOneSummaryByEmployeeId(id + '');
+    summary_salary.value = res.data[0];
 
-    console.log(res.data);
+    // console.log(res.data[0]);
   }
   const getOneEmployee = async (id: string) => {
     loadingStore.isLoading = true;
--- src/views/EmployeeLogin copy.vue
+++ src/views/EmployeeLogin copy.vue
@@ -1,10 +1,11 @@
+
 <script setup lang="ts">
-import ConfirmDialog from '@/components/ConfirmDialog.vue'
+import ConfirmDialog from '@/components/ConfirmDialog.vue';
 import { useEmployeeStore } from '@/store/employee.store'
 import type Employee from '@/store/types/employee.type'
-import type User from '@/store/types/user.type'
+
 import { onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute } from 'vue-router';
 import type { VForm } from 'vuetify/components'
 const url = import.meta.env.VITE_URL_PORT
 const employeeStore = useEmployeeStore()
@@ -12,110 +13,91 @@ const valid = ref(true)
 const form = ref<InstanceType<typeof VForm> | null>(null)
 const email = ref('')
 const name = ref('')
-const loading = ref(false)
+const loading = ref(false);
 const data = ref(JSON.parse(JSON.stringify(localStorage.getItem('employee'))))
 const employee = ref<Employee>(JSON.parse(data.value))
-const route = useRoute()
-const id = ref(route.params.id)
+const route = useRoute();
+const id = ref(route.params.id);
 
 onMounted(async () => {
-await employeeStore.getOneEmployee(employee.value.id + '')
-console.log(employeeStore.editEmployee)
-})
+    await employeeStore.getOneEmployee(employee.value.id + '')
+    console.log(employeeStore.editEmployee)
+});
 </script>
 
 <template>
-<ConfirmDialog ref="confirmDlg"></ConfirmDialog>
-<v-container>
-<v-card>
-<v-card-text width="100vw" style="height: 90vh">
-<v-row class="text-center" cols="12">
-<v-col class="title" md="4">
-<v-container style="height: 100%">
-<v-row class="profile" style="height: 30%">
-<v-col>
-<v-container>
-<v-avatar size="100"
-><v-img :src="`${url}/employees/image/${employee.image}`"></v-img
-></v-avatar>
-<div class="text-subtitle-2 mt-3">{{ employee.name }}</div></v-container
->
-</v-col>
-</v-row>
-<hr style="border: 2px solid #e4dccf; border-radius: 50px" />
-<v-row style="height: 50%">
-<v-col class="detail-emp">
-<div style="text-align: left">
-<p>📛 Name : {{ employee.name }}</p>
-<p>📨 Email : {{ employee.email }}</p>
-<p>🗃️ Position : {{ employee.position }}</p>
-</div>
-</v-col>
-</v-row>
-<v-row class="button" style="height: 10%">
-<v-col>
-<v-btn
-class="mdi mr-2 mdi-close-circle"
-style="float: right; color: white"
-color="red"
-@click="employeeStore.empCheckOut"
->
-Checkin</v-btn
->
+    <ConfirmDialog ref="confirmDlg"></ConfirmDialog>
+    <v-container >
+        <v-card style="background-color: #E9EDC9">
+            <v-card-title>
+                Employee
+                <v-spacer> </v-spacer>
+            </v-card-title>
+            <v-card-text>
+                <v-table class="text-center mt-5" style="background-color: #E9EDC9">
+                    <v-row>
+                        <v-col>
+                            <v-row>
+                                <v-col style="justify-content: center">
+                                    <v-avatar size="100"><v-img
+                                            :src="`${url}/employees/image/${employee.image}`"></v-img></v-avatar>
+                                    <div class="text-subtitle-2 mt-3">{{ employee.name }}</div>
 
-<v-btn
-class="mdi mr-2 mdi-checkbox-marked-circle"
-style="float: right; color: white"
-color="green"
-@click="employeeStore.empCheckIn"
->Checkin</v-btn
->
-</v-col>
-</v-row>
-</v-container>
-</v-col>
+                                </v-col>
 
-<v-col class="detail">
-<v-container>
-<VTable class="text-center mt-5" style="justify-content: center; overflow-y: auto">
-<thead style="justify-content: center; overflow-y: auto">
-<tr>
-<th>Time in</th>
-<th>Time out</th>
-<th>Total hour</th>
-</tr>
-</thead>
-<tbody>
-<tr
-class="text-center mr-5"
-style="justify-content: center"
-v-for="(item, index) in employeeStore.editEmployee.check_in_outs"
-:key="index"
->
-<td>{{ item.time_in }}</td>
-<td>{{ item.time_out }}</td>
-<td>{{ item.total_hour }}</td>
-</tr>
-</tbody>
-</VTable>
-</v-container>
-</v-col>
-</v-row>
-</v-card-text>
-</v-card>
-</v-container>
+                            </v-row>
+
+
+                            <v-row style="justify-content: center ">
+                                <div>
+                                    <v-card style="justify-content: center" width="400" text="Pofile">
+                                        <v-card-title style="justify-content: center">
+                                            <div class="text-center" style="font-size: 75%;">
+                                                <p>Name : {{ employee.name }}</p>
+                                                <p>Email : {{ employee.email }}</p>
+                                                <p>Position : {{ employee.position }}</p>
+                                            </div>
+                                        </v-card-title>
+                                    </v-card>
+                                </div>
+                            </v-row>
+                            <v-row>
+                                <v-col class="switch">
+
+                                    <v-btn class="mdi mr-2 mdi-checkbox-marked-circle" style="float: right; color: white"
+                                        color="green" @click="employeeStore.empCheckIn">Checkin</v-btn>
+
+                                </v-col>
+                            </v-row>
+
+                        </v-col>
+                    </v-row>
+
+                    <VTable class="text-center mt-5" style="justify-content: center; overflow-y: auto;">
+
+                        <thead style="justify-content: center; overflow-y: auto;">
+
+                            <tr>
+                                <th>Time in</th>
+                                <th>Time out</th>
+                                <th>Total hour</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="text-center mr-5 " style="justify-content: center"
+                                v-for="(item, index) in employeeStore.editEmployee.check_in_outs" :key="index">
+                                <td>{{ item.time_in }}</td>
+                                <td>{{ item.time_out }}</td>
+                                <td>{{ item.total_hour }}</td>
+                            </tr>
+                        </tbody>
+
+                    </VTable>
+                </v-table>
+            </v-card-text>
+        </v-card>
+    </v-container>
 </template>
-<style scoped>
-.title {
-background-color: rgb(250, 235, 215, 0.5);
-height: 100vh;
-}
-.detail-emp {
-width: 100%;
-display: flex;
-justify-content: center;
-}
-.detail {
-width: 70vw;
-}
-</style>
+<style></style>
+
