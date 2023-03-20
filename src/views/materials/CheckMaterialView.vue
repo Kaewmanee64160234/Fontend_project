fe8237a13b87413025eb6cb7b75941d15748f843
Thanawuth Rodcharoen <64160049@go.buu.ac.th>
2023-03-20 21:00:40 +0700

    finished check materail creat style

--- src/views/materials/CheckMaterialView.vue
+++ src/views/materials/CheckMaterialView.vue
@@ -1,108 +1,353 @@
-<<<<<<< HEAD
-<script lang="ts" setup>
-import type checkmaterial from '@/services/checkmaterial';
+
+<script setup lang="ts">
+
 import { useCheckMaterialStore } from '@/store/checkmaterial.store';
-import { useCheckMaterailDetailStore } from '@/store/checkmaterialdetail.store';
-import { onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+const checkmaterialStore = useCheckMaterialStore();
 
 const route = useRoute()
 const id = route.params.id
-const checkmaterialStore = useCheckMaterialStore()
 onMounted(() => {
-    checkmaterialStore.getOneCheckMatrial(id + '')
-=======
-<script setup lang="ts">
-import { useBillStore } from '@/store/bill.store';
-import { onMounted, ref } from 'vue';
-import { useRoute } from 'vue-router';
-const billStore = useBillStore();
+  checkmaterialStore.getCheckMaterail()
 
-onMounted(() => {
-  billStore.getBills()
->>>>>>> 2eaa9b70f0ecb3969068d6356c88b99005d4e65b
 })
 
 </script>
-<<<<<<< HEAD
 <template >
-    <table class="body-wrap">
-      <tbody>
-        <tr>
-          <td></td>
-          <td class="container" width="600">
-            <div class="content">
-              <table class="main" width="100%" cellpadding="0" cellspacing="0">
-                <tbody>
-                  <tr>
-                    <td class="content-wrap aligncenter">
-                      <table width="100%" cellpadding="0" cellspacing="0">
-                        <tbody>
-                          <tr>
-                            <td class="content-block">
-                            </td>
-                          </tr>
-                          <tr>
-                            <td class="content-block">
-                              <table class="invoice">
-                                <tbody>
-                                  <tr>
-                                    <td>employee id: {{ checkmaterialStore.checkMaterial.employeeId }}<br />Date :{{ checkmaterialStore.checkMaterial.createdAt }}<br /> time: {{ checkmaterialStore.checkMaterial.time }}</td>
-                                  </tr>
-                                  <tr>
-                                    <td>
-                                      <table class="invoice-items" cellpadding="0" cellspacing="0">
-                                        <tbody >
-                                          <tr v-for="item in checkmaterialStore.checkMaterial.checkmaterialdetails" :key="item.name">
-                                            <td>{{ item.name }}</td>
-                                            <td class="alignright">{{ item.checkmaterialID }} </td>
-                                            <td class="alignright">{{item.qty_last}} </td>
-                                            <td class="alignright">{{item.qty_remain}} </td>
-                                          </tr>
-                                          <tr class="finishOrder" >
-                                            <td>Name</td>
-                                     
-                                          </tr>
+  <table class="body-wrap">
+    <tbody>
+      <tr>
+        <td></td>
+        <td class="container" width="600">
+          <div class="content">
+            <table class="main" width="100%" cellpadding="0" cellspacing="0">
+              <tbody>
+                <tr>
+                  <td class="content-wrap aligncenter">
+                    <table width="100%" cellpadding="0" cellspacing="0">
+                      <tbody>
+                        <tr>
+                          <td class="content-block">
+                            <h2> Check Material </h2>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="content-block">
+                            <table class="invoice">
+                              <tbody>
+                               
+                                <tr>
+                                  <td>
+                                    <table class="invoice-items" cellpadding="0" cellspacing="0">
+                                      <tbody >
+                                        <tr >
+                                          <td>Material</td>
+                                          <td class="alignright"> ฿</td>
+                                        </tr>
+                                        <tr class="finishOrder" >
+                                          <td>Discount</td>
+                                          <td class="alignright"> ฿</td>
+                                        </tr>
+                                        <tr >
+                                          <td>Recieve</td>
+                                          <td class="alignright"> ฿</td>
+                                        </tr>
+                                        <tr >
+                                          <td>Change</td>
+                                          <td class="alignright"> ฿</td>
+                                        </tr>
+                                        
+                                        <tr class="total">
+                                          <td class="alignright" width="80%">Total</td>
+                                          <td class="alignright"> ฿</td>
                                           
-                                        </tbody>
-                                      </table>
-                                    </td>
-                                  </tr>
-                                </tbody>
-                              </table>
-                            </td>
-                          </tr>
-                         
-                          <tr>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              
-            </div>
-          </td>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
-  </template>
-=======
-<template>
-    <div>Check Material {{ id }}</div>
-    <v-container>
-        <v-card>
-            <v-card-title> BIll
-               <v-table>
-                <v-row>
-                    <v-col>
-                    </v-col>
-                </v-row>
-               </v-table>
-            </v-card-title>
-        </v-card>
-    </v-container>
+                                        </tr>
+                                      </tbody>
+                                    </table>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </td>
+                        </tr>
+                       
+                        <tr>
+                          <td class="content-block">
+                           All For One Company Inc. Buu section 2 .
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            
+          </div>
+        </td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
 </template>
->>>>>>> 2eaa9b70f0ecb3969068d6356c88b99005d4e65b
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+  box-sizing: border-box;
+  font-size: 14px;
+}
+
+img {
+  max-width: 100%;
+}
+
+body {
+  -webkit-font-smoothing: antialiased;
+  -webkit-text-size-adjust: none;
+  width: 100% !important;
+  height: 100%;
+  line-height: 1.6;
+}
+
+/* Let's make sure all tables have defaults */
+table td {
+  vertical-align: top;
+}
+
+/* -------------------------------------
+    BODY & CONTAINER
+------------------------------------- */
+body {
+  background-color: #ffffff;
+}
+
+.body-wrap {
+  width: 100vw;
+  height: 100vh;
+  background-color: #eee;
+}
+
+.container {
+  display: block !important;
+  max-width: 600px !important;
+  margin: 0 auto !important;
+  /* makes it centered */
+  clear: both !important;
+}
+
+.content {
+  max-width: 600px;
+  margin: 0 auto;
+  display: block;
+  padding: 20px;
+}
+
+/* -------------------------------------
+    HEADER, FOOTER, MAIN
+------------------------------------- */
+.main {
+  background: #fff;
+  border: 1px solid #e9e9e9;
+  border-radius: 3px;
+}
+
+.content-wrap {
+  padding: 20px;
+}
+
+.content-block {
+  padding: 0 0 20px;
+}
+
+.header {
+  width: 100%;
+  margin-bottom: 20px;
+}
+
+/* -------------------------------------
+    TYPOGRAPHY
+------------------------------------- */
+h1,
+h2,
+h3 {
+  font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif;
+  color: #000;
+  margin: 40px 0 0;
+  line-height: 1.2;
+  font-weight: 400;
+}
+
+h1 {
+  font-size: 32px;
+  font-weight: 500;
+}
+
+h2 {
+  font-size: 24px;
+}
+
+h3 {
+  font-size: 18px;
+}
+
+h4 {
+  font-size: 14px;
+  font-weight: 600;
+}
+
+p,
+ul,
+ol {
+  margin-bottom: 10px;
+  font-weight: normal;
+}
+p li,
+ul li,
+ol li {
+  margin-left: 5px;
+  list-style-position: inside;
+}
+
+/* -------------------------------------
+    LINKS & BUTTONS
+------------------------------------- */
+a {
+  color: #1ab394;
+  text-decoration: underline;
+}
+
+.btn-primary {
+  text-decoration: none;
+  color: #fff;
+  background-color: #1ab394;
+  border: solid #1ab394;
+  border-width: 5px 10px;
+  line-height: 2;
+  font-weight: bold;
+  text-align: center;
+  cursor: pointer;
+  display: inline-block;
+  border-radius: 5px;
+  text-transform: capitalize;
+}
+
+/* -------------------------------------
+    OTHER STYLES THAT MIGHT BE USEFUL
+------------------------------------- */
+.last {
+  margin-bottom: 0;
+}
+
+.first {
+  margin-top: 0;
+}
+
+.aligncenter {
+  text-align: center;
+}
+
+.alignright {
+  text-align: right;
+}
+
+.alignleft {
+  text-align: left;
+}
+
+.clear {
+  clear: both;
+}
+
+/* -------------------------------------
+    ALERTS
+    Change the class depending on warning email, good email or bad email
+------------------------------------- */
+.alert {
+  font-size: 16px;
+  color: #fff;
+  font-weight: 500;
+  padding: 20px;
+  text-align: center;
+  border-radius: 3px 3px 0 0;
+}
+.alert a {
+  color: #fff;
+  text-decoration: none;
+  font-weight: 500;
+  font-size: 16px;
+}
+.alert.alert-warning {
+  background: #f8ac59;
+}
+.alert.alert-bad {
+  background: #ed5565;
+}
+.alert.alert-good {
+  background: #1ab394;
+}
+
+/* -------------------------------------
+    INVOICE
+    Styles for the billing table
+------------------------------------- */
+.invoice {
+  margin: 40px auto;
+  text-align: left;
+  width: 80%;
+}
+.invoice td {
+  padding: 5px 0;
+}
+.invoice .invoice-items {
+  width: 100%;
+}
+.invoice .invoice-items td {
+  border-top: #eee 1px solid;
+}
+.invoice .invoice-items .total td {
+
+  font-weight: 700;
+}
+
+/* -------------------------------------
+    RESPONSIVE AND MOBILE FRIENDLY STYLES
+------------------------------------- */
+@media only screen and (max-width: 640px) {
+  h1,
+  h2,
+  h3,
+  h4 {
+    font-weight: 600 !important;
+    margin: 20px 0 5px !important;
+  }
+
+  h1 {
+    font-size: 22px !important;
+  }
+
+  h2 {
+    font-size: 18px !important;
+  }
+
+  h3 {
+    font-size: 16px !important;
+  }
+
+  .container {
+    width: 100% !important;
+  }
+
+  .content,
+  .content-wrap {
+    padding: 10px !important;
+  }
+
+  .invoice {
+    width: 100% !important;
+  }
+}
+</style>
+
--- src/views/materials/ShowBillView.vue
+++ src/views/materials/ShowBillView.vue
@@ -1,15 +1,32 @@
-<script setup lang="ts">import { ref } from 'vue';
-import { useRoute } from 'vue-router';
 
-const route = useRoute();
-const id = ref(route.params.id);
+<script setup lang="ts">
+import { useBillStore } from '@/store/bill.store';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+const billStore = useBillStore();
+
+const route = useRoute()
+const id = route.params.id
+onMounted(() => {
+  billStore.getBills()
+
+})
+
 </script>
 <template>
-    <div>Check Material ID: {{ id }}</div>
+    <div>Check Material {{ id }}</div>
     <v-container>
         <v-card>
-            <v-card-title> BILL
+            <v-card-title> BIll
+               <v-table>
+                <v-row>
+                    <v-col>
+                    </v-col>
+                </v-row>
+               </v-table>
             </v-card-title>
         </v-card>
     </v-container>
 </template>
+
+    
