7091f41cfcbc8c5507c7a9203bcd4368d2661acd
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-04-04 15:52:37 +0700

    create store useManageTime

--- src/views/ShowCio.vue
+++ src/views/ShowCio.vue
@@ -17,7 +17,13 @@ const data = ref(JSON.parse(JSON.stringify(localStorage.getItem('employee'))))
 const employee = ref<Employee>(JSON.parse(data.value))
 const route = useRoute()
 const id = ref(route.params.id)
-
+// const getDate = (timeDB:string,type:string) =>{
+//   if(type ==='Date'){
+//     return timeDB.substring()
+//   }else{
+//     return timeDB.getFullYear()
+//   }
+// }
 onMounted(async () => {
   await employeeStore.getOneSummarySalaryEmp(id.value + '')
 })
@@ -26,10 +32,11 @@ onMounted(async () => {
 <template>
   <ConfirmDialog ref="confirmDlg"></ConfirmDialog>
   <v-container>
-    <v-card>
-      <!-- {{ employeeStore.summary_salaries }} -->
+    <!-- {{ employeeStore.summary_salaries }} -->
+
+    <div v-for="(item, index) in employeeStore.summary_salaries" :key="index">
       <div
-        class="stats shadow"
+        class="summary stats shadow"
         style="background-color: white; box-shadow: rgba(0, 0, 0, 0.25) 0px 4px 5px"
       >
         <div class="stat">
@@ -37,7 +44,8 @@ onMounted(async () => {
             class="stat-value text-brown-lighten-1"
             style="font-size: 29px; font-family: sans-serif"
           >
-            2023 / 04
+            <!-- {{ getDate(item.ss_date?.toDateString+'','Date') }} -->
+            {{ new Date(item.ss_date+'').getUTCMonth() }}
           </div>
           <br />
           <div class="stat-desc" style="font-size: 15px">YYYY/MM</div>
@@ -70,154 +78,7 @@ onMounted(async () => {
           >
         </div>
       </div>
-      <div v-for="(item, index) in employeeStore.summary_salaries" :key="index">
-        <div class="stats shadow">
-          <div class="stat">
-            <div class="stat-figure text-primary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                class="inline-block w-8 h-8 stroke-current"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                ></path>
-              </svg>
-            </div>
-            <div class="stat-title">Total Likes</div>
-            <div class="stat-value text-primary">{{ item.hour }}</div>
-            <div class="stat-desc">21% more than last month</div>
-          </div>
-
-          <div class="stat">
-            <div class="stat-figure text-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                class="inline-block w-8 h-8 stroke-current"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M13 10V3L4 14h7v7l9-11h-7z"
-                ></path>
-              </svg>
-            </div>
-            <div class="stat-title">Page Views</div>
-            <div class="stat-value text-secondary">2.6M</div>
-            <div class="stat-desc">21% more than last month</div>
-          </div>
-
-          <div class="stat">
-            <div class="stat-figure text-secondary">
-              <div class="avatar online">
-                <div class="w-16 rounded-full">
-                  <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                </div>
-              </div>
-            </div>
-            <div class="stat-value">86%</div>
-            <div class="stat-title">Tasks done</div>
-            <div class="stat-desc text-secondary">31 tasks remaining</div>
-          </div>
-        </div>
-      </div>
-
-      <!-- <v-card-text width="100vw" style="height: 90vh ;">
-        <v-row class="text-center" cols="12">
-          <v-col class="title" md="4">
-            <v-container style="height: 100%">
-              <v-row class="profile" style="height: 25%">
-                <v-col>
-                  <v-container>
-                    <v-avatar size="100"><v-img :src="`${url}/employees/image/${employee.image}`"></v-img></v-avatar>
-                    <div class="text-subtitle-2 mt-3">{{ employeeStore.summary_salary.employee?.name }}</div>
-                  </v-container>
-                </v-col>
-              </v-row>
-
-              <v-row style="height: 30%">
-                <v-col>
-                  <v-card style="background-color: white">
-                    <v-card-text style="text-align: center">
-                      <p>👤 Name : {{ employeeStore.summary_salary.checkInOut[0].employee?.name }}</p>
-                      <br />
-                      <p>📩 Email: {{ employeeStore.summary_salary.checkInOut[0].employee?.email }}</p>
-                      <br />
-                      <p>🗃️ Position : {{ employeeStore.summary_salary.checkInOut[0].employee?.position }}</p>
-                      <br />
-                      <p>🕐 hourly : {{ employeeStore.summary_salary.checkInOut[0].employee?.hourly }} ฿</p>
-                    </v-card-text>
-                  </v-card>
-                </v-col>
-              </v-row>
-
-            </v-container>
-          </v-col>
-
-          <v-col class="detail">
-            <v-container style="height: 100%; ">
-              <v-row style="height: 13%">
-                <v-row class="text-center">
-                  <v-col class="detail-emp">
-                    <v-card height="100px" width="300px" style=" border-radius: 15px; background-color: #DEF5E5;">
-                      <v-card-title class="text-left">
-                        <h7> {{ employeeStore.summary_salary.salary }} ฿ </h7> <br />
-                        <h7 style="font-size: 15px; color: #6D9886">💸 Your Salary </h7>
-                      </v-card-title>
-                    </v-card>
-                  </v-col>
-
-                  <v-col class="detail-emp">
-                    <v-card height="100px" width="300px" style=" border-radius: 15px; background-color: #FFE3E1;">
-                      <v-card-title class="text-left">
-                        <h7> {{ employeeStore.summary_salary.hour }} hour </h7> <br />
-                        <h7 style="font-size: 15px; color: #FF9494">🕒 Total work </h7>
-                      </v-card-title>
-                    </v-card>
-                  </v-col>
-                </v-row>
-              </v-row>
-              <v-col class="detail">
-                <v-container style="height: 60%; ">
-                  <VTable  fixed-header height="600px" class="text-center mt-5" style="justify-content: center; overflow-y: auto;">
-                    <thead style="justify-content: center; ">
-                      <tr>
-                        <th>Time in</th>
-                        <th>Time out</th>
-                        <th>Total hour</th>
-                      </tr>
-                    </thead>
-                    <tbody style=" overflow-y: auto;">
-                      <tr class="text-center mr-5" style="justify-content: center  overflow-y: auto;"
-                        v-for="(item, index) in employeeStore.summary_salary.checkInOut" :key="index">
-                        <td>{{ item.time_in }}</td>
-                        <td>{{ item.time_out }}</td>
-                        <td>{{ item.total_hour }}</td>
-                      </tr>
-                    </tbody>
-                  </VTable>
-
-                </v-container>
-              </v-col>
-
-              <v-container width="100%" justify="center">
-
-                <v-pagination justify="center" v-model="employeeStore.page" :length="employeeStore.lastPage"
-                  rounded="circle"></v-pagination>
-
-              </v-container>
-            </v-container>
-          </v-col>
-        </v-row>
-      </v-card-text> -->
-    </v-card>
+    </div>
   </v-container>
 </template>
 <style scoped>
