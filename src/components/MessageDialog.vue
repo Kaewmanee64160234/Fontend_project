1006b124bfdfbf5fb7b31fd510b453b90735c553
Titapon Keawbua <118767390+64160047@users.noreply.github.com>
2023-04-05 09:17:30 +0700

    fix messagedialoge icon

--- src/components/MessageDialog.vue
+++ src/components/MessageDialog.vue
@@ -5,9 +5,8 @@ const messageStore = useMessageStore();
 </script>
 <template>
   <v-snackbar v-model="messageStore.isShow" multi-line color="black" rounded="pill" >
-    <div style="text-align: center; font-size: 14px;"><v-icon  class="mr-5" > mdi-information-slab-circle-outline</v-icon>  {{ messageStore.message }}</div>
+    <div  style="text-align: center; font-size: 14px;"><v-icon class="mr-2"><br/>mdi-bell-alert-outline</v-icon>  {{ messageStore.message }}</div>
    
-
     <template v-slot:actions>
       <v-btn color="white" variant="text" @click="messageStore.isShow = false">
         ยกเลิก
