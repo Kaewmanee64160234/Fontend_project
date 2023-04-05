5cfdc728e6a25188f4a23b252e4cfee9927a123d
Manita Intharachot <64160144@go.buu.ac.th>
2023-04-05 14:45:11 +0700

    Edit ShowBill

--- src/views/materials/ShowBillView.vue
+++ src/views/materials/ShowBillView.vue
@@ -1,7 +1,6 @@
 <script lang="ts" setup>
-import bill from '@/services/bill';
 import { useBillStore } from '@/store/bill.store';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 const billStore = useBillStore();
 
 onMounted(async () => {
@@ -10,12 +9,18 @@ onMounted(async () => {
 </script>
 <template>
     <v-container>
-        <v-card>
-            <v-card-title>
-                Show Bill
-            </v-card-title>
-            <v-card class="text-center">
-                <v-table>
+        <v-card class="text-center mb-10" color="#F9F5E7" v-for="(item, index) of billStore.bill" :key="index" style="widows: 500;">
+            <v-card-title>Bill</v-card-title>
+            <v-card-text>{{ item.name }}</v-card-text>
+            <v-card-text>{{ item.buy }}</v-card-text>
+            <v-card-text>{{ item.change }}</v-card-text>
+            <v-card-text>{{ item.total }}</v-card-text>
+        </v-card>
+    </v-container>
+</template>
+
+
+  <!-- <v-table>
                     <thead>
                         <tr>
                             <th>ID</th>
@@ -25,25 +30,16 @@ onMounted(async () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <!-- {{ billStore.bill_list }} -->
                         <tr v-for="(item, index) of billStore.bill" :key="index">
                             <td>{{ index + 1 }}</td>
                             <td>{{ item.name }}</td>
-                            <!-- <td>{{ item.id }}</td> 
-                        <td>{{ item.name }}</td> -->
-                            <td>{{ new Date(item.date + '').getDate() + '/' + new Date(item.date + '').getMonth() + '/' + new
-                                Date(item.date + '').getFullYear() }}</td>
+                            <td>{{ new Date(item.date + '').getDate() + '/' + new Date(item.date + '').getMonth() + '/' + new Date(item.date + '').getFullYear() }}</td>
                             <td>{{ item.total }}</td>
-
                         </tr>
                     </tbody>
-                    <tbody v-if="billStore.bill_Detail_List.length == 0">
+                    <tbody v-if="billStore.bill.length === 0">
                         <tr>
                             <td colspan="7" class="text-center">No data</td>
                         </tr>
                     </tbody>
-                </v-table>
-            </v-card>
-        </v-card>
-    </v-container>
-</template>
+                </v-table> -->
