19c5e32de392dd244528f1479e48fc09ddc61350
Thanawuth Rodcharoen <64160049@go.buu.ac.th>
2023-04-08 10:32:49 +0700

    fixed check materail but not done yet

--- src/store/checkmaterial.store.ts
+++ src/store/checkmaterial.store.ts
@@ -64,7 +64,7 @@ export const useCheckMaterialStore = defineStore("checkmaterail", () => {
       console.log(page.value)
       const response = await checkmaterialService.getOneCheckMaterail(id);
       console.log(response.data)
-      checkmeterialDetail.value = response.data
+      checkmeterialDetail.value = response.data[0]
       console.log(checkmeterialDetail.value)
     } catch (err) {
       console.log(err)
