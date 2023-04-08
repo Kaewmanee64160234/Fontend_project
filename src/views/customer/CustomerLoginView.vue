95d2018c4b082c4fe9a93b3a3ba662db508ff3e0
Titapon Keawbua <118767390+64160047@users.noreply.github.com>
2023-04-08 22:18:50 +0700

    ui checkpoint dialog

--- src/views/customer/CustomerLoginView.vue
+++ src/views/customer/CustomerLoginView.vue
@@ -1,32 +1,36 @@
 <script setup lang="ts">
-import CheckPromotionDialog from '@/components/promotion/CheckPromotionDialog.vue';
-import { usePointOfSale } from '@/store/pointOfSell.store'
-
-const pointOfSaleStore = usePointOfSale()
+import CheckPointDialog from '@/components/CheckPointDialog.vue'
+import { useCustomerStore } from '@/store/customer.store'
+const customerStore = useCustomerStore()
 </script>
 
 <template>
+    <CheckPointDialog></CheckPointDialog>
     <v-container>
-        <CheckPromotionDialog></CheckPromotionDialog>
         <v-row justify="center">
             <v-col>
-                <div class="mockup-phone">
-                    <div class="camera"></div>
-                    <div class="display">
-                        <div class="artboard artboard-demo phone-1">
-                            <div>
-                                <v-btn justify-center class="mdi mr-2 mdi-checkbox-marked-circle ma-6 text-center " style="float: right; color: white;"
-                                color="#D5B4B4" width="90%">Check Point</v-btn>
+                <v-col>
+                    <div class="mockup-phone">
+                        <div class="camera"></div>
+                        <div class="display">
+                            <div class="artboard artboard-demo phone-1">
+                                <div>
+                                    <v-btn justify-center class="mdi mr-2 mdi-checkbox-marked-circle ma-6 text-center "
+                                        style="float: right; color: white;" color="#D5B4B4" width="90%"
+                                        @click="customerStore.dialogCheckPoint = true">Check Point</v-btn>
+                                </div>
+                                <div>
+                                    <v-btn justify-center class="mdi mr-2 mdi-checkbox-marked-circle text-center"
+                                        style="float: right; color: white ;" color="#D5B4B4" width="90%">Check
+                                        Promotion</v-btn>
+                                </div>
+
                             </div>
-                            <div>
-                                <v-btn justify-center class="mdi mr-2 mdi-checkbox-marked-circle text-center" style="float: right; color: white ;"
-                                color="#D5B4B4" width="90%" @click="pointOfSaleStore.dialogCheckPromotion = true"> Check Promotion
-                                </v-btn>
-                            </div>
-                            
                         </div>
                     </div>
-                </div>
+
+                </v-col>
+
             </v-col>
         </v-row>
     </v-container>
