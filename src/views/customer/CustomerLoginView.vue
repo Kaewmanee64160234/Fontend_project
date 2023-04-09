59f6e731845cecf401bfca1b73f0f6ee5e5de238
Pornchitar Sarsai <kream21574@gmail.com>
2023-04-09 13:40:48 +0700

    fix button in CustomerLoginView

--- src/views/customer/CustomerLoginView.vue
+++ src/views/customer/CustomerLoginView.vue
@@ -18,16 +18,34 @@ const pointOfSaleStore = usePointOfSale()
                         <div class="camera"></div>
                         <div class="display">
                             <div class="artboard artboard-demo phone-1">
-                                <div>
-                                    <v-btn justify-center class="mdi mr-2 mdi-checkbox-marked-circle ma-6 text-center "
-                                        style="float: right; color: white;" color="#D5B4B4" width="90%"
-                                        @click="customerStore.dialogCheckPoint = true">Check Point</v-btn>
-                                </div>
-                                <div>
-                                    <v-btn justify-center class="mdi mr-2 mdi-checkbox-marked-circle text-center"
-                                        style="float: right; color: white ;" color="#D5B4B4" width="90%" @click="pointOfSaleStore.dialogCheckPromotion = true">Check
-                                        Promotion</v-btn>
-                                </div>
+                                <v-container>
+                                    <v-row>
+                                        <v-col>
+                                            <div>
+                                                <v-btn block rounded="xl" justify-center class="mdi  mdi-checkbox-marked-circle text-center "
+                                                    style="float: right; color: white;" color="#D5B4B4" width="80px" height="40px"
+                                                    @click="customerStore.dialogCheckPoint = true">Check Point</v-btn>
+                                            </div>
+                                        </v-col>
+                                        
+                                    </v-row>
+                                    
+                                </v-container>
+                                <v-container>
+                                    <v-row>
+                                        <v-col>
+                                            <div>
+                                                <v-btn block rounded="xl" justify-center class="mdi  mdi-checkbox-marked-circle text-center"
+                                                    style="float: right; color: white ;" color="#D5B4B4" width="80px" height="40px" @click="pointOfSaleStore.dialogCheckPromotion = true">Check
+                                                    Promotion</v-btn>
+            
+                                            </div>
+                                        </v-col>
+                                        
+                                    </v-row>
+                                    
+                                </v-container>
+                                
 
                             </div>
                         </div>
